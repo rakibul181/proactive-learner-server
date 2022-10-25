4289cc357b7a89280236936d37781fe08986feed
rakibul181 <rakibulhasan0641@gmail.com>
2022-10-25 21:49:17 +0600

    Cors install

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
-
 const port = process.env.PORT || 5000
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('Coueses Api Is Running')
