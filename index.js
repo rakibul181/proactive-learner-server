4125842d5a63253300342d5974eeb122dcca7711
rakibul181 <rakibulhasan0641@gmail.com>
2022-10-26 13:20:44 +0600

    Single couser API link update

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.get('/courses',(req, res)=>{
     res.send(courses)
 })
 
+app.get('/course/:id',(req, res)=>{
+    id = req.params.id 
+    const selectedCourse = courses.find(course=> course.id === id)
+    console.log(selectedCourse);
+    res.send(selectedCourse)
+})
+
 app.listen(port,()=>{
     console.log('Courses Server is RUnning');
 })
